1dc451e2563eedf177182b7877fdf7eb188a4be3
BunhengG <bbmc255@gmail.com>
2025-01-28 17:08:03 +0700

    Update

--- lib/providers/playlist_provider.dart
+++ lib/providers/playlist_provider.dart
@@ -38,6 +38,51 @@ class PlaylistProvider extends ChangeNotifier {
       albumName: "Skull III",
       albumArtImagePath: "assets/images/s4.png",
       audioPath: "audio/SANGKRAN_MAGIC.mp3",
+    ),
+
+    // Song 5
+    Song(
+      title: "KHMER GENTLEMEN",
+      artistName: "VANTHAN FT VANNDA",
+      albumName: "",
+      albumArtImagePath: "assets/images/s5.png",
+      audioPath: "audio/Khmer_Gentlemen.mp3",
+    ),
+
+    // Song 6
+    Song(
+      title: "POINT OF NO RETURN",
+      artistName: "VANTHAN FT VANNDA",
+      albumName: "",
+      albumArtImagePath: "assets/images/s6.png",
+      audioPath: "audio/Point_of_No_Return.mp3",
+    ),
+
+    // Song 7
+    Song(
+      title: "UNBROKEN",
+      artistName: "SUZANA",
+      albumName: "",
+      albumArtImagePath: "assets/images/s7.png",
+      audioPath: "audio/SUZANA_UNBROKEN.mp3",
+    ),
+
+    // Song 8
+    Song(
+      title: "1NA",
+      artistName: "NORITH FT YUUHAI",
+      albumName: "",
+      albumArtImagePath: "assets/images/s8.png",
+      audioPath: "audio/1NA.mp3",
+    ),
+
+    // Song 9
+    Song(
+      title: "KRA MOAM SROK NA",
+      artistName: "NORITH",
+      albumName: "",
+      albumArtImagePath: "assets/images/s9.png",
+      audioPath: "audio/NORITH_ft_VANNDA.mp3",
     )
   ];
 
